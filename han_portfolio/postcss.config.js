fc4e6865b262a0f0d3d38a98faa2cec80f30ec2e
yihantan18 <yihantan18@gmail.com>
2025-08-07 11:45:44 +0800

    Delete han_portfolio directory

--- han_portfolio/postcss.config.js
+++
@@ -1,7 +0,0 @@
-// postcss.config.js (ESM-friendly and correct)
-import tailwindcss from 'tailwindcss';
-import autoprefixer from 'autoprefixer';
-
-export default {
-  plugins: [tailwindcss, autoprefixer],
-};
